eeaac9dc6a5aae986a9035238542b95bf344279a
Kyle Hench <kylehench@gmail.com>
2021-12-16 16:10:38 -0800

    FizzBuzz

---
+++ FizzBuzz/script.js
@@ -0,0 +1,14 @@
+for (var i = 1; i <= 100; i++) {
+    var stringOutput = ""
+    if (i % 3 === 0) {
+        stringOutput += "Fizz"
+    }
+    if (i % 5 === 0) {
+        stringOutput += "Buzz"
+    }
+    if (stringOutput.length === 0) {
+        console.log(i)
+    } else {
+        console.log(stringOutput)
+    }
+}
