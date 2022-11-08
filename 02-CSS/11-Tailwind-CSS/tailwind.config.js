93a0680f46a2041a2439f367b8cf5875d61cb198
Kyle Hench <kylehench@gmail.com>
2022-11-08 13:20:36 -0800

    changes to readme

--- 02-CSS/11-Tailwind-CSS/tailwind.config.js
+++ 02-CSS/11-Tailwind-CSS/tailwind.config.js
@@ -4,9 +4,16 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     screens: {
-      
+      sm: '480px',
+      md: '768px',
+      lg:'976px',
+      xl:'1440px'
     },
-    extend: {},
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+      }
+    },
   },
   plugins: [],
 }
